c2da0f9392b32622c51582b3095b960caf2c0857
Richard <rbarrera87@gmail.com>
2015-08-10 22:21:03 -0700

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name postApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the postApp
+ */
+angular.module('postApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('http://localhost:3000/api/posts')
+      .then(function(posts){
+        console.log(posts.data.posts);
+        $scope.posts = posts.data.posts;
+      });
+    $scope.addPost = function(){
+       console.log("adding");
+       $http.post('http://localhost:3000/api/posts', {post: {title: $scope.title, body: $scope.body}})
+        .then(function(post){
+          $scope.posts.push(post.data.post);
+          $scope.title = "";
+          $scope.body = "";
+        });
+    };
+    $scope.removePost = function(id){
+      console.log("Removing index number: " + id);
+      $http.delete('http://localhost:3000/api/posts/' + id)
+        .then(function(){
+          for (var i = $scope.posts.length - 1; i >= 0; i--) {
+            if($scope.posts[i].id === id){
+              $scope.posts.splice(i, 1);
+            }
+          }
+        });
+    };
+  });
